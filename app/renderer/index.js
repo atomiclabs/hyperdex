2ede6ca8e5a7e40d785bf5fec5dd80b730c9f276
Luke Childs <lukechilds123@gmail.com>
2018-01-09 01:02:12 +0700

    Add BarterDEX API client (#5)

--- app/renderer/index.js
+++ app/renderer/index.js
@@ -1,6 +1,7 @@
 import electron from 'electron';
 import React from 'react';
 import {render} from 'react-dom';
+import Api from './api';
 
 require('electron-unhandled')();
 
@@ -29,3 +30,17 @@ class App extends React.Component {
 }
 
 render(<App/>, document.querySelector('#app'));
+
+// TODO: This is only temporary for testing
+// Make sure BarterDEX is running first:
+// docker run -e PASSPHRASE="secure passphrase" -p 127.0.0.1:7783:7783 lukechilds/barterdex-api
+const PASSPHRASE = 'secure passphrase';
+const api = new Api({
+	endpoint: `http://localhost:7783`,
+	passphrase: PASSPHRASE
+});
+
+(async () => {
+	console.log('Portfolio:', await api.portfolio());
+	console.log('Coins:', (await api.coins())[0]);
+})();
