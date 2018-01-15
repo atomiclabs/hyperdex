93d3268065b2fdbd531acfdbabf0662b6af090bf
Luke Childs <lukechilds123@gmail.com>
2018-01-15 16:25:28 +0700

    Use crypto-random-string

--- app/portfolio.js
+++ app/portfolio.js
@@ -4,6 +4,7 @@ const {app} = require('electron');
 const iocane = require('iocane');
 const slug = require('slug');
 const filenamify = require('filenamify');
+const randomString = require('crypto-random-string');
 const writeJsonFile = require('write-json-file');
 const dir = require('node-dir');
 const loadJsonFile = require('load-json-file');
@@ -13,7 +14,7 @@ const portfolioPath = app.getPath('userData') + '/portfolios';
 
 const create = async ({name, seedPhrase, password}) => {
 	const safeName = filenamify(slug(name, {lower: true, symbols: false}));
-	const filename = `hyperdex-portfolio-${safeName}-${randomBytes(4).toString('hex')}.json`;
+	const filename = `hyperdex-portfolio-${safeName}-${randomString(6)}.json`;
 	const path = `${portfolioPath}/${filename}`;
 	const data = {
 		name,
