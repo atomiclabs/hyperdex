083a2b8c83392f573a20ef0252925f4b60d3a577
Luke Childs <lukechilds123@gmail.com>
2018-01-15 18:22:04 +0700

    Don't manually join file paths

--- app/portfolio.js
+++ app/portfolio.js
@@ -1,4 +1,5 @@
 'use strict';
+const path = require('path');
 const {app} = require('electron');
 const iocane = require('iocane');
 const slug = require('slug');
@@ -9,21 +10,21 @@ const dir = require('node-dir');
 const loadJsonFile = require('load-json-file');
 
 const {encryptWithPassword: encrypt, decryptWithPassword: decrypt} = iocane.crypto;
-const portfolioPath = app.getPath('userData') + '/portfolios';
+const portfolioPath = path.join(app.getPath('userData'), 'portfolios');
 
 const create = async ({name, seedPhrase, password}) => {
 	const safeName = filenamify(slug(name, {lower: true, symbols: false}));
 	const filename = `hyperdex-portfolio-${safeName}-${randomString(6)}.json`;
-	const path = `${portfolioPath}/${filename}`;
+	const filePath = path.join(portfolioPath, filename);
 	const data = {
 		name,
 		encryptedSeedPhrase: await encrypt(seedPhrase, password),
 		appVersion: app.getVersion(),
 	};
 
-	await writeJsonFile(path, data);
+	await writeJsonFile(filePath, data);
 
-	return path;
+	return filePath;
 };
 
 const getAll = async () => {
