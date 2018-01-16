6aa900d6ff97b95623edfaa04694e81fe7400cd8
Luke Childs <lukechilds123@gmail.com>
2018-01-16 19:51:30 +0700

    Login view

--- app/renderer/components/login.js
+++ app/renderer/components/login.js
@@ -1,18 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 /* eslint-disable */
 
-const Login = props => (
-	<div className="container">
-		<header className="dashhead">
-			<div className="dashhead-titles">
-				<h3 className="dashhead-title">Login</h3>
-			</div>
-		</header>
-		<main>
-			<p>TODO: Login form</p>
-		</main>
-	</div>
+const Login = ({match}) => (
+	<div className="Login container">
+		<div className="buttons">
+			<Link to={match.url + '/add-new'} className="btn btn-lg btn-primary btn-block">Create new portfolio</Link>
+			<Link to={match.url + '/add-existing'} className="btn btn-lg btn-primary btn-block">Add existing portfolio</Link>
+		</div>
+  </div>
 );
 
 export default Login;
