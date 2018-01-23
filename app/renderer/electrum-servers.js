b1852ae68d49c419a436ede6b132d8c1020c2e40
Luke Childs <lukechilds123@gmail.com>
2018-01-23 18:28:25 +0700

    Comma dangle

--- app/renderer/electrum-servers.js
+++ app/renderer/electrum-servers.js
@@ -1,15 +1,15 @@
 const electrumServers = {
 	KMD: {
 		ipaddr: 'electrum1.cipig.net',
-		port: 10001
+		port: 10001,
 	},
 	VTC: {
 		ipaddr: 'uk1.vtconline.org',
-		port: 55001
+		port: 55001,
 	},
 	LTC: {
 		ipaddr: '173.212.225.176',
-		port: 50012
+		port: 50012,
 	},
 };
 
