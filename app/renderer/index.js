0002199c259dbb1b17f4337f5b4ff25229bec31f
Sindre Sorhus <sindresorhus@gmail.com>
2018-01-31 12:32:23 +0700

    Missed a PascalCase'ing
    Since macOS is case-insensitive and Linux is not...

--- app/renderer/index.js
+++ app/renderer/index.js
@@ -1,7 +1,7 @@
 import electron from 'electron';
 import React from 'react';
 import {render} from 'react-dom';
-import App from './app';
+import App from './App';
 
 require('electron-unhandled')();
 
