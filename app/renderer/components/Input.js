c51ba0da3ffd65195ccce1cf31299da9f941e1da
Sindre Sorhus <sindresorhus@gmail.com>
2018-02-05 15:09:54 +0700

    Style inputs

--- app/renderer/components/Input.js
+++ app/renderer/components/Input.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import styled from 'styled-components';
 import {RSInput} from 'reactsymbols-kit';
 
 // See http://docs.reactsymbols.com/#/?id=rsinput
 
-const Input = styled(RSInput)`
+const Input = styled(props => (
+	<RSInput {...props} iconSize="16"/>
+))`
 	background: #222C3C !important;
 	color: pink;
 `;
