8def30871e0145ce79d7d6e445e3193bbc4ad8cd
Sindre Sorhus <sindresorhus@gmail.com>
2018-03-21 11:19:07 +0700

    [WIP] Add feedback button

---
+++ app/constants.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// TODO: Put website URL here
+exports.websiteUrl = 'https://github.com/lukechilds/hyperdex';
+
+exports.repoUrl = 'https://github.com/lukechilds/hyperdex';
--- app/renderer/views/TabView.js
+++ app/renderer/views/TabView.js
@@ -1,8 +1,11 @@
+import {remote} from 'electron';
 import React from 'react';
 import {appContainer} from 'containers/App';
 import avatar from '../avatar';
 import Nav from './Nav';
 import './TabView.scss';
+
+const {openGitHubIssue} = remote.require('./util');
 
 const TabView = props => (
 	<div className="TabView">
@@ -10,9 +13,20 @@ const TabView = props => (
 			<h1 className="app-name">
 				HyperDEX
 			</h1>
-			<div className="portfolio-dropdown">
-				<div className="avatar-wrapper">
-					<img src={avatar(appContainer.state.portfolio.name)}/>
+			<div className="right-container">
+				<button
+					type="button"
+					className="feedback-button"
+					onClick={() => {
+						openGitHubIssue('<!--\n\nWe appreciate your feedback!\nTry to include as much relevant info as possible.\n\n-->');
+					}}
+				>
+					Feedback
+				</button>
+				<div className="portfolio-dropdown">
+					<div className="avatar-wrapper">
+						<img src={avatar(appContainer.state.portfolio.name)}/>
+					</div>
 				</div>
 			</div>
 		</header>
