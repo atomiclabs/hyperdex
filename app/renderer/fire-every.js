3da681953072c6e30435206aa8226ef9ca22c244
Luke Childs <lukechilds123@gmail.com>
2018-03-23 15:05:03 +0700

    Normalize get currency

--- app/renderer/fire-every.js
+++ app/renderer/fire-every.js
@@ -1,13 +1,12 @@
-const fireEvery = (cb, delay) => {
+const fireEvery = (cb, delay) => new Promise(resolve => {
 	let timeoutId = 0;
 
 	(async function fire() {
 		const startTime = Date.now();
 		await cb();
+		resolve(() => clearTimeout(timeoutId));
 		timeoutId = setTimeout(fire, delay - (Date.now() - startTime));
 	})();
-
-	return () => clearTimeout(timeoutId);
-};
+});
 
 export default fireEvery;
