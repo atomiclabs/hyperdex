d7d2a4e65ce0c4c0da028fa30923b6fc99a2fd14
Luke Childs <lukechilds123@gmail.com>
2018-03-23 15:46:19 +0700

    Refactor fireEvery into async function

--- app/renderer/fire-every.js
+++ app/renderer/fire-every.js
@@ -1,12 +1,12 @@
-const fireEvery = (cb, delay) => new Promise(resolve => {
+const fireEvery = async (cb, delay) => {
 	let timeoutId = 0;
 
-	(async function fire() {
+	return (async function fire() {
 		const startTime = Date.now();
 		await cb();
-		resolve(() => clearTimeout(timeoutId));
 		timeoutId = setTimeout(fire, delay - (Date.now() - startTime));
+		return () => clearTimeout(timeoutId);
 	})();
-});
+};
 
 export default fireEvery;
