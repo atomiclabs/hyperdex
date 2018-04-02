c08f2aa2257cc2b0dd08fe80dae1bb6ed83616fb
Luke Childs <lukechilds123@gmail.com>
2018-04-02 13:51:33 +0700

    Add getSwap method

--- app/renderer/swap-db.js
+++ app/renderer/swap-db.js
@@ -7,10 +7,13 @@ class SwapDB {
 	constructor() {
 		this.db = new PouchDB('swaps', {adapter: 'idb'});
 
+		// This is a bug in PouchDB but to be able to sort via timeStarted it MUST
+		// be the fist item in the index.
+		// https://github.com/pouchdb/pouchdb/issues/6399#issuecomment-361938113
 		this.ready = this.db.createIndex({
 			index: {
-				fields: ['tradeId', 'aliceId', 'requestId', 'quoteId', 'timeStarted']
-			}
+				fields: ['timeStarted', 'tradeId', 'aliceId', 'requestId', 'quoteId'],
+			},
 		});
 	}
 
@@ -36,8 +39,33 @@ class SwapDB {
 		return this.db.post(formattedSwap);
 	}
 
-	updateSwap(swap) {
+	async getSwap(ids) {
+		const {tradeId, aliceId, requestId, quoteId} = ids;
 
+		const query = {};
+		if (requestId && quoteId) {
+			query.selector = {requestId, quoteId};
+		} else if (tradeId && aliceId) {
+			query.selector = {tradeId, aliceId};
+		}
+
+		if (!query.selector) {
+			throw new Error(`Swap must have tradeId/aliceId or requestId/quoteId: ${JSON.stringify(ids)}`);
+		}
+
+		// We need `{$gt: true}` so PouchDB can sort.
+		// https://github.com/pouchdb/pouchdb/issues/6399#issuecomment-295858705
+		query.selector.timeStarted = {$gt: true};
+		query.sort = [{timeStarted: 'desc'}];
+
+		const {docs} = await this.db.find(query);
+
+		if (docs.length > 1) {
+			const collisionProps = Object.keys(query.selector).filter(p => p !== 'timeStarted').join('/');
+			console.warn(`swapDB ${collisionProps} collision detected!`, {query, docs});
+		}
+
+		return docs[0];
 	}
 }
 
