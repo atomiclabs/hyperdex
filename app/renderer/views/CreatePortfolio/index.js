417187a7b68d47eb2129cc4905b84dc228dc8f92
Sindre Sorhus <sindresorhus@gmail.com>
2018-04-23 15:20:11 +0700

    Add missing 4th step in the CreatePortfolio flow

--- app/renderer/views/CreatePortfolio/index.js
+++ app/renderer/views/CreatePortfolio/index.js
@@ -5,6 +5,7 @@ import createPortfolioContainer from 'containers/CreatePortfolio';
 import Step1 from './Step1';
 import Step2 from './Step2';
 import Step3 from './Step3';
+import Step4 from './Step4';
 
 const CreatePortfolio = () => (
 	<Subscribe to={[createPortfolioContainer]}>
@@ -13,6 +14,7 @@ const CreatePortfolio = () => (
 				<LoginView component={Step1}/>
 				<LoginView component={Step2}/>
 				<LoginView component={Step3}/>
+				<LoginView component={Step4}/>
 			</React.Fragment>
 		)}
 	</Subscribe>
