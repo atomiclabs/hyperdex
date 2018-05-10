35fa9c03e68671a8cbfc45a4f6122078bfe1e903
Luke Childs <lukechilds123@gmail.com>
2018-05-10 16:43:12 +0700

    List coins in preferences and save selection to config file

--- app/renderer/views/Preferences.js
+++ app/renderer/views/Preferences.js
@@ -1,7 +1,10 @@
 import electron from 'electron';
 import React from 'react';
 import _ from 'lodash';
+import coinlist from 'coinlist';
 import Input from 'components/Input';
+import CurrencyIcon from 'components/CurrencyIcon';
+import supportedCurrencies from '../../marketmaker/supported-currencies';
 import TabView from './TabView';
 import './Preferences.scss';
 
@@ -10,6 +13,7 @@ const config = electron.remote.require('./config');
 class Form extends React.Component {
 	state = {
 		marketmakerUrl: config.get('marketmakerUrl') || '',
+		enabledCoins: config.get('enabledCoins') || [],
 	};
 
 	persistState = _.debounce((name, value) => {
@@ -20,6 +24,20 @@ class Form extends React.Component {
 		const {name} = event.target;
 		this.setState({[name]: value});
 		this.persistState(name, value);
+	};
+
+	toggleCurrency = (value, event) => {
+		const {checked} = event.target;
+
+		this.setState(prevState => {
+			const enabledCoins = checked ?
+				[...prevState.enabledCoins, value] :
+				prevState.enabledCoins.filter(coin => coin !== value);
+
+			this.persistState('enabledCoins', enabledCoins);
+
+			return {enabledCoins};
+		});
 	};
 
 	render() {
@@ -35,6 +53,23 @@ class Form extends React.Component {
 						onChange={this.handleChange}
 						placeholder="Example: http://localhost:7783"
 					/>
+				</div>
+				<div className="form-group">
+					<label>
+						Enabled Currencies:
+					</label>
+					{supportedCurrencies.map(currency => (
+						<label key={currency.coin} style={{display: 'block'}}>
+							<CurrencyIcon symbol={currency.coin}/>
+							{(coinlist.get(currency.coin, 'name') || currency.coin)} ({currency.coin})
+							<Input
+								type="checkbox"
+								value={currency.coin}
+								checked={this.state.enabledCoins.includes(currency.coin)}
+								onChange={this.toggleCurrency}
+							/>
+						</label>
+					))}
 				</div>
 			</React.Fragment>
 		);
