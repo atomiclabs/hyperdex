138d15da91eab83f7453d3141671738f4bf9a3ac
Sindre Sorhus <sindresorhus@gmail.com>
2018-05-14 18:23:06 +0700

    Add initial Swap view styling

--- app/renderer/views/Swap.js
+++ app/renderer/views/Swap.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import TabView from './TabView';
+import './Swap.scss';
 
-export default class Swap extends React.Component {
+class Swap extends React.Component {
 	render() {
 		return (
-			<TabView title="Swap">
-				Not yet implemented
+			<TabView title="Swap" className="Swap--wrapper">
+				<div className="Swap">
+					<div className="temp-message">
+						<h1>Coming soon…</h1>
+						<p>Simple atomic swaps with minimal user interaction</p>
+					</div>
+				</div>
 			</TabView>
 		);
 	}
 }
+
+export default Swap;
