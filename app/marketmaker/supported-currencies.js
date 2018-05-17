adff629f3f9f6951329e8d59500e3723a90b7674
Sindre Sorhus <sindresorhus@gmail.com>
2018-05-17 13:30:33 +0700

    Add PIZZA and BEER test currencies (#225)

--- app/marketmaker/supported-currencies.js
+++ app/marketmaker/supported-currencies.js
@@ -167,6 +167,38 @@ const supportedCurrencies = [
 			},
 		],
 	},
+	{
+		coin: 'PIZZA',
+		name: 'Pizza',
+		asset: 'PIZZA',
+		rpcport: 11116,
+		electrumServers: [
+			{
+				host: 'electrum1.cipig.net',
+				port: 10024,
+			},
+			{
+				host: 'electrum2.cipig.net',
+				port: 10024,
+			},
+		],
+	},
+	{
+		coin: 'BEER',
+		name: 'Beer',
+		asset: 'BEER',
+		rpcport: 8923,
+		electrumServers: [
+			{
+				host: 'electrum1.cipig.net',
+				port: 10022,
+			},
+			{
+				host: 'electrum2.cipig.net',
+				port: 10022,
+			},
+		],
+	},
 ];
 
 const getCurrencySymbols = () => _.orderBy(supportedCurrencies.map(currency => currency.coin));
