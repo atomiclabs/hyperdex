ed32cbc3ea95134bb52a3581ff53207b8192c001
Sindre Sorhus <sindresorhus@gmail.com>
2018-05-21 13:54:36 +0700

    Sort existing currencies alphabetically

--- app/marketmaker/supported-currencies.js
+++ app/marketmaker/supported-currencies.js
@@ -5,15 +5,18 @@ const _ = require('lodash');
 
 const supportedCurrencies = [
 	{
-		coin: 'KMD',
+		coin: 'BEER',
+		name: 'Beer',
+		asset: 'BEER',
+		rpcport: 8923,
 		electrumServers: [
 			{
 				host: 'electrum1.cipig.net',
-				port: 10001,
+				port: 10022,
 			},
 			{
 				host: 'electrum2.cipig.net',
-				port: 10001,
+				port: 10022,
 			},
 		],
 	},
@@ -27,6 +30,140 @@ const supportedCurrencies = [
 			{
 				host: 'electrum2.cipig.net',
 				port: 10000,
+			},
+		],
+	},
+	{
+		coin: 'CHIPS',
+		name: 'Chips',
+		rpcport: 57776,
+		pubtype: 60,
+		p2shtype: 85,
+		wiftype: 188,
+		txfee: 10000,
+		electrumServers: [
+			{
+				host: 'electrum1.cipig.net',
+				port: 10053,
+			},
+			{
+				host: 'electrum2.cipig.net',
+				port: 10053,
+			},
+		],
+	},
+	{
+		coin: 'COQUI',
+		name: 'Coqui Cash',
+		asset: 'COQUI',
+		rpcport: 14276,
+		electrumServers: [
+			{
+				host: 'electrum1.cipig.net',
+				port: 10011,
+			},
+			{
+				host: 'electrum2.cipig.net',
+				port: 10011,
+			},
+		],
+	},
+	{
+		coin: 'DGB',
+		rpcport: 14022,
+		pubtype: 30,
+		p2shtype: 5,
+		wiftype: 128,
+		txfee: 100000,
+		electrumServers: [
+			{
+				host: 'electrum1.cipig.net',
+				port: 10059,
+			},
+			{
+				host: 'electrum2.cipig.net',
+				port: 10059,
+			},
+		],
+	},
+	{
+		coin: 'DOGE',
+		rpcport: 22555,
+		pubtype: 30,
+		p2shtype: 22,
+		wiftype: 158,
+		txfee: 100000000,
+		electrumServers: [
+			{
+				host: 'electrum1.cipig.net',
+				port: 10060,
+			},
+			{
+				host: 'electrum2.cipig.net',
+				port: 10060,
+			},
+		],
+	},
+	{
+		coin: 'KMD',
+		electrumServers: [
+			{
+				host: 'electrum1.cipig.net',
+				port: 10001,
+			},
+			{
+				host: 'electrum2.cipig.net',
+				port: 10001,
+			},
+		],
+	},
+	{
+		coin: 'LTC',
+		rpcport: 9332,
+		pubtype: 48,
+		p2shtype: 5,
+		wiftype: 176,
+		txfee: 100000,
+		electrumServers: [
+			{
+				host: 'electrum1.cipig.net',
+				port: 10065,
+			},
+			{
+				host: 'electrum2.cipig.net',
+				port: 10065,
+			},
+		],
+	},
+	{
+		coin: 'OOT',
+		name: 'Utrum',
+		asset: 'OOT',
+		rpcport: 12467,
+		electrumServers: [
+			{
+				host: 'electrum1.utrum.io',
+				port: 10088,
+			},
+			{
+				host: 'electrum2.utrum.io',
+				port: 10088,
+			},
+		],
+	},
+	{
+		coin: 'PIZZA',
+		name: 'Pizza',
+		asset: 'PIZZA',
+		rpcport: 11116,
+		electrumServers: [
+			{
+				host: 'electrum1.cipig.net',
+				port: 10024,
+			},
+			{
+				host: 'electrum2.cipig.net',
+				port: 10024,
 			},
 		],
 	},
@@ -63,57 +200,6 @@ const supportedCurrencies = [
 		],
 	},
 	{
-		coin: 'OOT',
-		name: 'Utrum',
-		asset: 'OOT',
-		rpcport: 12467,
-		electrumServers: [
-			{
-				host: 'electrum1.utrum.io',
-				port: 10088,
-			},
-			{
-				host: 'electrum2.utrum.io',
-				port: 10088,
-			},
-		],
-	},
-	{
-		coin: 'COQUI',
-		name: 'Coqui Cash',
-		asset: 'COQUI',
-		rpcport: 14276,
-		electrumServers: [
-			{
-				host: 'electrum1.cipig.net',
-				port: 10011,
-			},
-			{
-				host: 'electrum2.cipig.net',
-				port: 10011,
-			},
-		],
-	},
-	{
-		coin: 'CHIPS',
-		name: 'Chips',
-		rpcport: 57776,
-		pubtype: 60,
-		p2shtype: 85,
-		wiftype: 188,
-		txfee: 10000,
-		electrumServers: [
-			{
-				host: 'electrum1.cipig.net',
-				port: 10053,
-			},
-			{
-				host: 'electrum2.cipig.net',
-				port: 10053,
-			},
-		],
-	},
-	{
 		coin: 'VTC',
 		rpcport: 5888,
 		pubtype: 71,
@@ -128,92 +214,6 @@ const supportedCurrencies = [
 			{
 				host: 'electrum2.cipig.net',
 				port: 10071,
-			},
-		],
-	},
-	{
-		coin: 'LTC',
-		rpcport: 9332,
-		pubtype: 48,
-		p2shtype: 5,
-		wiftype: 176,
-		txfee: 100000,
-		electrumServers: [
-			{
-				host: 'electrum1.cipig.net',
-				port: 10065,
-			},
-			{
-				host: 'electrum2.cipig.net',
-				port: 10065,
-			},
-		],
-	},
-	{
-		coin: 'DOGE',
-		rpcport: 22555,
-		pubtype: 30,
-		p2shtype: 22,
-		wiftype: 158,
-		txfee: 100000000,
-		electrumServers: [
-			{
-				host: 'electrum1.cipig.net',
-				port: 10060,
-			},
-			{
-				host: 'electrum2.cipig.net',
-				port: 10060,
-			},
-		],
-	},
-	{
-		coin: 'PIZZA',
-		name: 'Pizza',
-		asset: 'PIZZA',
-		rpcport: 11116,
-		electrumServers: [
-			{
-				host: 'electrum1.cipig.net',
-				port: 10024,
-			},
-			{
-				host: 'electrum2.cipig.net',
-				port: 10024,
-			},
-		],
-	},
-	{
-		coin: 'BEER',
-		name: 'Beer',
-		asset: 'BEER',
-		rpcport: 8923,
-		electrumServers: [
-			{
-				host: 'electrum1.cipig.net',
-				port: 10022,
-			},
-			{
-				host: 'electrum2.cipig.net',
-				port: 10022,
-			},
-		],
-	},
-	{
-		coin: 'DGB',
-		rpcport: 14022,
-		pubtype: 30,
-		p2shtype: 5,
-		wiftype: 128,
-		txfee: 100000,
-		electrumServers: [
-			{
-				host: 'electrum1.cipig.net',
-				port: 10059,
-			},
-			{
-				host: 'electrum2.cipig.net',
-				port: 10059,
 			},
 		],
 	},
