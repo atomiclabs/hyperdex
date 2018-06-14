49714fea4fe0f84cae5d76b13a6f53beb5b9ad24
Sindre Sorhus <sindresorhus@gmail.com>
2018-06-14 13:27:15 +0700

    Disable Electron security warnings
    We're already following all of them, but not in dev as we don't have CSP enabled there.

--- app/renderer/index.js
+++ app/renderer/index.js
@@ -5,6 +5,8 @@ import {Provider} from 'unstated';
 import UNSTATED from 'unstated-debug';
 import App from './views/App';
 
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
+
 UNSTATED.isEnabled = is.development;
 UNSTATED.logStateChanges = false;
 
