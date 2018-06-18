atomiclabs/hyperdex

commit 54e5d286e11af8e8a26edda453e4e9a43c99ee0e

Don't prevent pasting of multiline strings
The `<TextArea>` component prevents newlines so the user won't write a seed phrase with accidental newlines, but it also prevented pasting a seed phrase with accidental newlines. Now it just cleans up the pasted seed phrase instead.

Fixes #316

app/renderer/components/TextArea.js
@@ -8,17 +8,21 @@ class TextArea extends React.Component {
 	};
 
 	handleChange = event => {
-		const {target} = event;
-		const {value} = target;
 		const {onChange, preventNewlines} = this.props;
+		const {target} = event;
+		let {value} = target;
 
-		if (preventNewlines && /\r?\n/.test(value)) {
-			const form = target.closest('form');
-			if (form) {
-				form.dispatchEvent(new Event('submit'));
+		if (preventNewlines) {
+			if (event.nativeEvent.inputType === 'insertLineBreak') {
+				const form = target.closest('form');
+				if (form) {
+					form.dispatchEvent(new Event('submit'));
+				}
+
+				return;
 			}
 
-			return;
+			value = value.replace(/\r?\n/g, ' ').trim();
 		}
 
 		if (onChange) {

commit 856c9715b99596dbabfbebb373b9886f185cf25b

Fix not being able to type space in the seed phrase input
Fixes #367

The regression was accidentally introduced in https://github.com/lukechilds/hyperdex/commit/54e5d286e11af8e8a26edda453e4e9a43c99ee0e

I'm just removing the `.trim()` as it was too opinionated anyway.

app/renderer/components/TextArea.js
@@ -22,7 +22,7 @@ class TextArea extends React.Component {
 				return;
 			}
 
-			value = value.replace(/\r?\n/g, ' ').trim();
+			value = value.replace(/\r?\n/g, ' ');
 		}
 
 		if (onChange) {