d7d789a5040d3b09c5a51392c78d49f140feed0d
Kevin Martensson <kevinmartensson@gmail.com>
2018-06-21 08:42:27 +0200

    Add translations for `ForgotPassword` and `RestorePortfolio` views

--- app/renderer/views/ForgotPassword/Step1.js
+++ app/renderer/views/ForgotPassword/Step1.js
@@ -3,7 +3,10 @@ import Button from 'components/Button';
 import TextArea from 'components/TextArea';
 import LoginBackButton from 'components/LoginBackButton';
 import container from 'containers/ForgotPassword';
+import {translate} from '../../translate';
 import './ForgotPassword.scss';
+
+const t = translate('forgot-password');
 
 const ForgotPasswordStep1 = () => {
 	const {state} = container;
@@ -11,13 +14,13 @@ const ForgotPasswordStep1 = () => {
 	return (
 		<div className="ForgotPassword">
 			<LoginBackButton view="LoginBox" progress={0}/>
-			<h1>Enter Your Seed Phrase</h1>
+			<h1>{t('enterSeedPhrase')}</h1>
 			<p>TODO: Put some explanation here on what to do.</p>
 			<div className="form-group" style={{width: '460px'}}>
 				<TextArea
 					value={state.seedPhrase}
 					onChange={container.handleSeedPhraseInputChange}
-					placeholder="Example: advanced generous profound …"
+					placeholder={t('exampleSeedPhrase', {seedPhrase: 'advanced generous profound'})}
 					autoFocus
 					required
 					preventNewlines
@@ -28,7 +31,7 @@ const ForgotPasswordStep1 = () => {
 			<div className="form-group">
 				<Button
 					primary
-					value="Confirm"
+					value={t('confirm')}
 					disabled={!state.seedPhrase}
 					onClick={container.handleClickConfirmSeedPhrase}
 					style={{width: '172px', marginTop: '18px'}}
