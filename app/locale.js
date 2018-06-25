fd5e58f7adb2c7fbad6e6b37188d43cce2b18509
Kevin Martensson <kevinmartensson@gmail.com>
2018-06-25 19:25:56 +0200

    Improve performance by using `ipc` instead of `remote.require`

--- app/locale.js
+++ app/locale.js
@@ -1,6 +1,7 @@
 'use strict';
 const path = require('path');
 const {app} = require('electron');
+const ipc = require('electron-better-ipc');
 const i18next = require('i18next');
 const i18nextBackend = require('i18next-sync-fs-backend');
 const {isDevelopment} = require('./util-common');
@@ -36,6 +37,8 @@ i18next
 app.on('ready', () => {
 	i18next.changeLanguage(app.getLocale());
 });
+
+ipc.answerRenderer('get-translations', () => i18next);
 
 exports.i18n = i18next;
 exports.translate = namespaces => namespaces ? i18next.getFixedT(null, namespaces) : i18next.t.bind(i18next);
--- app/renderer/translate.js
+++ app/renderer/translate.js
@@ -1,9 +1,20 @@
 import {remote} from 'electron';
-import {init} from 'i18next';
+import ipc from 'electron-better-ipc';
+import {createInstance, init} from 'i18next';
+import mem from 'mem';
 
-const {i18n} = remote.require('./locale');
-const instance = init({...i18n.options, lng: remote.app.getLocale(), resources: i18n.store.data});
-const translate = namespaces => namespaces ? instance.getFixedT(null, namespaces) : instance.t.bind(instance);
+const instance = createInstance();
+const translate = namespaces => mem(namespaces ? instance.getFixedT(null, namespaces) : instance.t.bind(instance));
+
+(async () => {
+	const i18n = await ipc.callMain('get-translations');
+
+	instance.init({
+		...i18n.options,
+		lng: remote.app.getLocale(),
+		resources: i18n.store.data,
+	});
+})();
 
 export {
 	instance,
