2d9df30ce001aa63acc16006f5d3206f4548db5d
Sindre Sorhus <sindresorhus@gmail.com>
2018-07-09 16:23:40 +0700

    Add ChainZilla (ZILLA) currency (#409)

--- app/marketmaker/supported-currencies.js
+++ app/marketmaker/supported-currencies.js
@@ -937,6 +937,22 @@ const supportedCurrencies = [
 			},
 		],
 	},
+	{
+		coin: 'ZILLA',
+		name: 'ChainZilla',
+		asset: 'ZILLA',
+		rpcport: 10041,
+		electrumServers: [
+			{
+				host: 'electrum1.cipig.net',
+				port: 10028,
+			},
+			{
+				host: 'electrum2.cipig.net',
+				port: 10028,
+			},
+		],
+	},
 ];
 
 const getCurrencySymbols = () => _.orderBy(supportedCurrencies.map(currency => currency.coin));
