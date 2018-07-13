711265b45fb92eb057e7b903e12de4ee782b94cb
Sindre Sorhus <sindresorhus@gmail.com>
2018-07-13 19:38:54 +0700

    Remove MYTH

--- app/marketmaker/supported-currencies.js
+++ app/marketmaker/supported-currencies.js
@@ -1048,13 +1048,6 @@ const supportedCurrencies = [
 		rpcport: 80,
 	},
 	{
-		coin: 'MYTH',
-		name: 'Unicoin',
-		decimals: 18,
-		etomic: '0x277ab4b9dde09a8e710fd755deeb9d0d9532d047',
-		rpcport: 80,
-	},
-	{
 		coin: 'NAS',
 		etomic: '0x5d65d971895edc438f465c17db6992698a52318d',
 		rpcport: 80,
