0020687821718a26b59783b46268daffd46a8c9d
Sindre Sorhus <sindresorhus@gmail.com>
2018-07-20 18:30:09 +0700

    Move the swap modal progressbar to the top of the modal
    Per request in #177

--- app/renderer/components/SwapDetails.js
+++ app/renderer/components/SwapDetails.js
@@ -120,6 +120,13 @@ class SwapDetails extends React.Component {
 					width="660px"
 				>
 					<React.Fragment>
+						<Progress
+							value={swap.progress}
+							color={
+								(swap.status === 'completed' && 'var(--success-color)') ||
+								(swap.status === 'failed' && 'var(--error-color)')
+							}
+						/>
 						<div className="section overview">
 							<div className="from">
 								<CurrencyIcon symbol={overview.fromCurrency}/>
@@ -134,13 +141,6 @@ class SwapDetails extends React.Component {
 							</div>
 						</div>
 						<div className="section progress">
-							<Progress
-								value={swap.progress}
-								color={
-									(swap.status === 'completed' && 'var(--success-color)') ||
-									(swap.status === 'failed' && 'var(--error-color)')
-								}
-							/>
 							<p>
 								{title(swap.statusFormatted)}
 								{(swap.status === 'failed' && swap.error) && (
