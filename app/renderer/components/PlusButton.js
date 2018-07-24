9c5abb679c5994e53992a8d35b9b123d3a01c86e
Sindre Sorhus <sindresorhus@gmail.com>
2018-07-24 07:01:02 +0700

    Get rid of Sass variables
    And disallow using them in the future. We should prefer native CSS variables as they work live which is needed for theming.

--- app/renderer/components/PlusButton.js
+++ app/renderer/components/PlusButton.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import PlusIcon from 'icons/Plus';
 import './PlusButton.scss';
 
-const PlusButton = ({className, ...props}) => <button {...props} type="button" className={`${className} PlusButton`}/>;
+const PlusButton = ({className, ...props}) => (
+	<button {...props} type="button" className={`${className} PlusButton`}>
+		<PlusIcon size="7px"/>
+	</button>
+);
 
 export default PlusButton;
---
+++ app/renderer/icons/Plus.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Svg from 'components/Svg';
+
+const Plus = props => (
+	<Svg {...props} viewBox="0 0 6 6">
+		<path d="M2.41 6H3.6V3.54H6V2.46H3.59V0H2.4v2.46H0v1.09h2.41z"/>
+	</Svg>
+);
+
+export default Plus;
