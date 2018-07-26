637c9d19d2c719f8995db624680dcdf2833f0cce
Sindre Sorhus <sindresorhus@gmail.com>
2018-07-26 13:24:47 +0700

    Add hotkey to restart HyperDEX in debug mode (#431)

--- app/util-common.js
+++ app/util-common.js
@@ -1,10 +1,11 @@
 'use strict';
 const {api} = require('electron-util');
+/// const config = require('./config');
 
 const isNightlyBuild = api.app.getName() === 'HyperDEX Nightly';
 
 /// TODO: Change this before the official launch
-/// const isDevelopment = is.development || isNightlyBuild;
+/// const isDevelopment = config.get('isDebugMode') || is.development || isNightlyBuild;
 const isDevelopment = true;
 
 module.exports = {
