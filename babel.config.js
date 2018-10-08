ff8476d9446a8c64b42c46c8e621cd9b5de8d53f
Kevin Martensson <kevinmartensson@gmail.com>
2018-10-08 22:34:44 +0200

    Upgrade to `ava@1.0.0-beta.8`

---
+++ babel.config.js
@@ -0,0 +1,56 @@
+'use strict';
+const path = require('path');
+
+const srcPath = path.join(__dirname, 'app', 'renderer');
+const plugins = [
+	[
+		'styled-jsx/babel',
+		{
+			vendorPrefixes: false,
+		},
+	],
+	'@babel/plugin-transform-modules-commonjs',
+	'@babel/plugin-syntax-dynamic-import',
+	'@babel/plugin-proposal-object-rest-spread',
+	[
+		'@babel/plugin-proposal-class-properties',
+		{
+			loose: false,
+		},
+	],
+	[
+		'module-resolver', {
+			alias: {
+				components: path.join(srcPath, 'components'),
+				containers: path.join(srcPath, 'containers'),
+				views: path.join(srcPath, 'views'),
+				icons: path.join(srcPath, 'icons'),
+			},
+		},
+	],
+];
+
+module.exports = {
+	presets: [
+		'@babel/preset-react',
+	],
+	plugins: [
+		...plugins,
+		'react-hot-loader/babel',
+	],
+	env: {
+		test: {
+			plugins: [
+				...plugins,
+				[
+					'transform-require-ignore',
+					{
+						extensions: [
+							'.scss',
+						],
+					},
+				],
+			],
+		},
+	},
+};
