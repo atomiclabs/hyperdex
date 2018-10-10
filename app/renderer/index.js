fd9d696fc95a49e0e6fb01fbd648582c8184d027
Sindre Sorhus <sindresorhus@gmail.com>
2018-10-10 15:21:07 +0700

    Silence `react-select` proptype warning
    Will be fixed when upgrading to `react-select` v2, but we don't have time to do that right now.

--- app/renderer/index.js
+++ app/renderer/index.js
@@ -3,10 +3,15 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'unstated';
 import UNSTATED from 'unstated-debug';
+import filterConsole from 'filter-console';
 import App from './views/App';
 import touchBar from './touch-bar';
 
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
+
+filterConsole([
+	'Warning: React does not recognize the `clearableValue` prop on a DOM element.',
+]);
 
 UNSTATED.isEnabled = is.development;
 UNSTATED.logStateChanges = false;
