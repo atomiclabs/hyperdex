12b61a4e97068b4af69c0e5d3541fb04bd15fa66
Sindre Sorhus <sindresorhus@gmail.com>
2018-10-23 00:22:59 +0700

    Update CRW and CHAIN Electrum servers

--- app/marketmaker/supported-currencies.js
+++ app/marketmaker/supported-currencies.js
@@ -436,12 +436,16 @@ const supportedCurrencies = [
 		rpcport: 15587,
 		electrumServers: [
 			{
-				host: 'electrum1.chainmakers.co',
-				port: 55417,
+				host: 'electrum1.cipig.net',
+				port: 10032,
 			},
 			{
-				host: 'electrum2.chainmakers.co',
-				port: 55417,
+				host: 'electrum2.cipig.net',
+				port: 10032,
+			},
+			{
+				host: 'electrum3.cipig.net',
+				port: 10032,
 			},
 		],
 	},
@@ -529,31 +533,35 @@ const supportedCurrencies = [
 		txfee: 10000,
 		electrumServers: [
 			{
-				host: 'blr-crwseed.crowndns.info',
+				host: 'blr-crwseed.infernopool.com',
 				port: 50001,
 			},
 			{
-				host: 'sfo-crwseed.crowndns.info',
+				host: 'sfo-crwseed.infernopool.com',
 				port: 50001,
 			},
 			{
-				host: 'nyc-crwseed.crowndns.info',
+				host: 'nyc-crwseed.infernopool.com',
 				port: 50001,
 			},
 			{
-				host: 'ams-crwseed.crowndns.info',
+				host: 'ams-crwseed.infernopool.com',
 				port: 50001,
 			},
 			{
-				host: 'tor-crwseed.crowndns.info',
+				host: 'tor-crwseed.infernopool.com',
 				port: 50001,
 			},
 			{
-				host: 'lon-crwseed.crowndns.info',
+				host: 'lon-crwseed.infernopool.com',
 				port: 50001,
 			},
 			{
-				host: 'fra-crwseed.crowndns.info',
+				host: 'fra-crwseed.infernopool.com',
+				port: 50001,
+			},
+			{
+				host: 'sgp-crwseed.infernopool.com',
 				port: 50001,
 			},
 		],
