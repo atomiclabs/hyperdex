8bc9e4f9712bff404561b53544925b82c284991f
Sindre Sorhus <sindresorhus@gmail.com>
2018-10-24 17:49:53 +0700

    Fix typo

--- app/index.js
+++ app/index.js
@@ -100,7 +100,7 @@ function createMainWindow() {
 	});
 
 	if (is.development) {
-		win.loadURL('http://localhost:80800/dev.html');
+		win.loadURL('http://localhost:8080/dev.html');
 	} else {
 		loadUrl(win);
 	}
